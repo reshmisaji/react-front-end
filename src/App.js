b584bb63d57d47f717efc38a9d47563b20069fb5
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2019-04-17 12:44:51 +0530

    Initial commit of front end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={flowers:[]}
+  }
+
+  componentDidMount(){
+    fetch('/flowers').then(res=>res.json()).then(res=>this.setState({flowers:res}))
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+     <div>{this.state}</div>
     );
   }
 }
